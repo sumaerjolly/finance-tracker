0326d15f78f5b26ccf0495d19683dd594a33161a
Sumaer Jolly <sumaerjolly@gmail.com>
2019-09-10 23:19:28 +0530

    Added functionality for invalid ticker entries

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,17 @@
 class StocksController < ApplicationController
 
     def search
-        @stock = Stock.new_from_lookup(params[:stock])
-        render 'users/my_portfolio'
+        if params[:stock].present?
+            @stock = Stock.new_from_lookup(params[:stock])
+            if @stock
+                render 'users/my_portfolio'
+            else 
+                flash[:danger] = "You have entered an incorrect stock ticker"
+                redirect_to my_portfolio_path
+            end 
+        else 
+            flash[:danger] = "You have not entered a stock ticker. Please enter one"
+            redirect_to my_portfolio_path
+        end 
     end
 end
