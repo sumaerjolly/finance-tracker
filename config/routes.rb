ed2bc6cb30ef75da6a6aeea422802f10a66bec1d
Sumaer Jolly <sumaerjolly@gmail.com>
2019-09-10 23:05:31 +0530

    Add stock lookup functionality on my portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'users/my_portfolio'
   devise_for :users
   root 'welcome#index'
-  get 'my_portfolio', to: "users#my_portfolio"
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stocks', to: 'stocks#search'
 end
