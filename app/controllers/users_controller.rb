6c04c8af13c0c47a0a3b0241c50ed2c90db42317
Sumaer Jolly <sumaerjolly@gmail.com>
2019-09-11 22:23:17 +0530

    Add search action and error implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,15 @@ class UsersController < ApplicationController
     @friendships = current_user.friends
   end 
 
-  def search 
+  def search
+    if params[:search_param].blank?
+      flash.now[:danger] = "You have to enter something to search"
+    else
+      @users = User.search(params[:search_param])
+      flash.now[:danger] = "No users match this search criteria" if @users.blank?
+    end
+    respond_to do |format|
+      format.js { render partial: 'friends/result' }
+    end
   end
 end
