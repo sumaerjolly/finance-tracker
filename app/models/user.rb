b2621534a282efd15258da0f09bc9b8c0e3e0e34
Sumaer Jolly <sumaerjolly@gmail.com>
2019-09-11 20:23:47 +0530

    Added friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
   has_many :stocks, through: :user_stocks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
