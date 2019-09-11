6eb482a1d68f60680e325fedc53aa94c0fcef85b
Sumaer Jolly <sumaerjolly@gmail.com>
2019-09-11 20:44:54 +0530

    display current friends in my friends page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
 
   def my_friends
+    @friendships = current_user.friends
   end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get 'search_stocks', to: 'stocks#search'
   resources :user_stocks, only: [:create, :destroy]
   get 'my_friends', to: "users#my_friends"
+  resources :users, only: [:show]
+  resources :friendships
 end
